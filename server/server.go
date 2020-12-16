4307f0cd8073d8d5f3cd3540ae39200c4469b170
Aslak <51925794+AslakOffi@users.noreply.github.com>
2020-12-16 12:18:59 +0100

    Update server.go

--- server/server.go
+++ server/server.go
@@ -16,7 +16,7 @@ var buf [512]byte
 func startServer(port string) error {
 	ln, err := listen(port)
 	if err != nil {
-		return fmt.Errorf("\nunable to start server: %v\n", err)
+		return fmt.Errorf("\nuimpossible de démarrer le serveur: %v\n", err)
 	}
 	err = acceptConnections(ln)
 	return err
@@ -25,9 +25,9 @@ func startServer(port string) error {
 func listen(port string) (net.Listener, error) {
 	ln, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		return nil, fmt.Errorf("\nunable to listen on port: %s because of listen error: %v", port, err)
+		return nil, fmt.Errorf("\nimpossible de trouver le port: %s en raison d'une erreur: %v", port, err)
 	}
-	log.Println("Listening on address: " + ln.Addr().String())
+	log.Println("Ecoute sur adresse: " + ln.Addr().String())
 	return ln, nil
 }
 
@@ -35,36 +35,36 @@ func acceptConnections(ln net.Listener) error {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Println("Failed to accept connection request.")
+			log.Println("Échec de l'acceptation à la demande de connexion.")
 			continue
 		}
-		log.Println("Client connected with address: " + conn.RemoteAddr().String())
+		log.Println("Client connecté avec l'adresse: " + conn.RemoteAddr().String())
 		go handleClient(conn)
 	}
 }
 
 func handleClient(conn net.Conn) {
-	log.Println("Handling client...")
+	log.Println("Traitement client...")
 
 	clientName, err := getClientName(conn)
 	if err != nil {
-		log.Println("Error with connection: " + conn.RemoteAddr().String() + ": " + err.Error())
+		log.Println("Erreur de connexion: " + conn.RemoteAddr().String() + ": " + err.Error())
 		return
 	}
 	if clientName == "!q\n" {
 		sendMessage(conn, "!q\n")
-		log.Println("Closing connection...")
+		log.Println("Fermeture de la connexion...")
 		err := conn.Close()
 		if err != nil {
-			log.Println("Closing connection failed.")
+			log.Println("La fermeture de la connexion a échoué.")
 		}
-		log.Println("Connection with address: " + conn.RemoteAddr().String() + " closed.")
+		log.Println("Connexion avec l'adresse: " + conn.RemoteAddr().String() + " fermée.")
 		return
 	}
 
 	err = echoMessages(conn, clientName)
 	if err != nil {
-		log.Println("Error with connection: + " + conn.RemoteAddr().String() + ": " + err.Error())
+		log.Println("Erreur de connexion: + " + conn.RemoteAddr().String() + ": " + err.Error())
 		return
 	}
 
@@ -73,7 +73,7 @@ func handleClient(conn net.Conn) {
 
 func getClientName(conn net.Conn) (string, error) {
 	clientName := ""
-	sendMessage(conn, "You are connected to the server, choose a username. Press esc to quit.")
+	sendMessage(conn, "Vous êtes connecté au serveur, choisissez un nom d'utilisateur. Appuyez sur Echap pour quitter.")
 
 	for {
 		receivedName, err := receiveMessage(conn)
@@ -94,14 +94,14 @@ func getClientName(conn net.Conn) (string, error) {
 			clientName = receivedName
 			break
 		}
-		sendMessage(conn, "The name is already taken, please choose another one.")
+		sendMessage(conn, "Le nom est déjà pris, veuillez en choisir un autre.")
 	}
 
-	sendMessage(conn, "Welcome to the room, "+clientName)
+	sendMessage(conn, "Bienvenue dans la room, "+clientName)
 	lock.RLock()
 	for name, conn := range clients {
 		if name != clientName {
-			go sendMessage(conn, clientName+" joined the room.")
+			go sendMessage(conn, clientName+" a joint la room.")
 		}
 	}
 	lock.RUnlock()
@@ -118,7 +118,7 @@ func echoMessages(conn net.Conn, clientName string) error {
 			delete(clients, clientName)
 			lock.Unlock()
 
-			broadcastMessage(clientName + " left the room.")
+			broadcastMessage(clientName + " a quitté la room.")
 			return err
 		}
 		if message == "!q\n" {
@@ -131,18 +131,18 @@ func echoMessages(conn net.Conn, clientName string) error {
 }
 
 func closeConnection(conn net.Conn, clientName string) {
-	log.Println("Closing connection...")
+	log.Println("Déconnexion...")
 	err := conn.Close()
 	if err != nil {
-		log.Println("Closing connection failed.")
+		log.Println("La déconnexion a échoué")
 	}
-	log.Println("Connection with address: " + conn.RemoteAddr().String() + " closed.")
+	log.Println("Connexion avec l'adresse: " + conn.RemoteAddr().String() + " fermée.")
 
 	lock.Lock()
 	delete(clients, clientName)
 	lock.Unlock()
 
-	broadcastMessage(clientName + " left the room.")
+	broadcastMessage(clientName + " a quitté la room.")
 }
 
 func broadcastMessage(message string) {
@@ -156,7 +156,7 @@ func broadcastMessage(message string) {
 func sendMessage(conn net.Conn, message string) {
 	_, err := conn.Write([]byte(message))
 	if err != nil {
-		log.Println("Cannot write to connection.")
+		log.Println("Impossible d'écrire sur cette connexion.")
 	}
 }
 
@@ -172,7 +172,7 @@ func receiveMessage(conn net.Conn) (message string, err error) {
 func main() {
 	var port string
 
-	flag.StringVar(&port, "port", "8001", "port on which the server will listen for new connections")
+	flag.StringVar(&port, "port", "8001", "port sur lequel le serveur verra les nouvelles connexions")
 	flag.Parse()
 
 	err := startServer(port)
